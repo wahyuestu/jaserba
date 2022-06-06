b0d97278403acbce1dce9aa71f74202ee4be9a4f
wahyuestu <106475443+wahyuestu@users.noreply.github.com>
2022-06-06 17:11:56 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load' , function () {
+    const erepair = document.querySelector('.erepair');
+    const pic = document.querySelector('.pic');
+
+    erepair.classList.add('show1');
+    pic.classList.add('show2');
+})
